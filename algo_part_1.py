80ee655535d6ad18784dae293e775141c704b852
Allison Shi <allisonshi5@gmail.com>
2024-05-31 12:00:42 -0600

    test_2

---
+++ algo_part_1.py
@@ -0,0 +1,25 @@
+s = "ABCBBCABABAC"
+n = len(s)
+A = [0] * n
+
+for k in range(0, n):
+    for a in range(k-1, -1, -1):
+        if s[a] == s[k]:
+            print("k: " + str(k), "& a: " + str(a))
+            t = A[a]
+            c = a + 1
+            while (t < k) and (c < k):
+                x = min(A[c], t)
+                y = max(A[c], t)
+                A[c] = x
+                t = y
+                c += 1
+            A[a] = k
+            print(A)
+
+for i in range(len(A)):
+    if A[i] != 0:
+        A[i] += 1
+
+print(A)
+    
