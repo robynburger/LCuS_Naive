505694e43df46440b8b10364b287cdbc2040d861
Allison Shi <allisonshi5@gmail.com>
2024-06-04 16:24:46 -0600

    edited nonconsec_two_case

--- simpler_cases/nonconsec_two_case.py
+++ simpler_cases/nonconsec_two_case.py
@@ -5,17 +5,23 @@ whole_seq = "abcadbab"  # longest seq is 2 - ab
 def helper(seq):
     n = len(seq)
     # T is 0...n-1 for each set of indices
-    T = [[[0 for x in range(n)] for y in range(n)] for z in range(n)]
+    T = [[[0 for x in range(n+1)] for y in range(n+1)] for z in range(n+1)]
     # populates T
-    f(seq, T)
+    T = f(seq, T)
     print(T)
 
 # populates T table
 def f(seq, T):
     n = len(seq)
     # seq is 0...n-1  --> with n indices
-    for k in range(1, n+1):
-        for i in range(1, n+1):
-            for j in range(i+1, n+1):
-                if seq[i-1] == seq[k-1]:
-                    T[k][i][j] = T[k-1][i-1][j]
+    for k in range(n):     # 0...n-1
+        for i in range(n):     # 0...n-1
+            for j in range(i+1, n):      # i+1...n-1
+                if seq[i] == seq[k]:        # 0...n-1
+                    T[i][j][k] = T[i-1][j][k-1] + 1
+                else:
+                    T[i][j][k] = max(T[i-1][j][k], T[i][j][k-1])
+    return T
+
+helper(whole_seq)
+                    
