80c4978c6757f9667b6e95b756654b5d5b133aa5
Allison Shi <allisonshi5@gmail.com>
2024-06-05 16:16:29 -0600

    new_max func implemented

--- nonconsec_cubic.py
+++ nonconsec_cubic.py
@@ -1,13 +1,30 @@
 # Given a string s of length n, LCS(s) returns the length of the LCS 
 import numpy as np
+import math
 
 def LCS(s):
   n = len(s)
-  T = np.zeros((n+1, n+1, n+1, n+1, n+1))
-  f(T, s)
-  return np.max(T)
+  T = np.zeros((n+1, n+1, n+1, n+1, n+1), dtype=int)
+  T = f(T, s)
+  return new_max(T, s)
 
 # LOOP OVER P, Q in T and find those that are the max
+def new_max(T, s):
+  n = len(s)
+  m = n
+  d = dict()
+
+  for i in range(1, m):
+    for k in range(i+1, m):
+      if T[m][i][i+1][k][k+1] in d.keys():
+        d[T[m][i][i+1][k][k+1]].append((i, k))
+      else:
+        d[T[m][i][i+1][k][k+1]] = [(i, k)]
+  
+  max_f = max(d.keys())
+  return d[max_f]
+
+# Populates values of tensor T 
 def f(T, s):
   n = len(s)
   for m in range(1, n+1):
@@ -18,7 +35,10 @@ def f(T, s):
             if s[i-1] == s[k-1] == s[m-1]: 
               T[m][i][j][k][l] = T[m-1][i-1][j][k-1][l] + 1 
             else:
-                T[m][i][j][k][l]= max(T[m][i-1][j][k][l], 
+                T[m][i][j][k][l]= max( T[m][i-1][j][k][l], 
                                               T[m][i][j][k-1][l], 
                                               T[m-1][i][j][k][l])
-print(LCS("aabcabbcfabc"))
+  return T
+
+ans = LCS("aabcabbcfabc")
+print(ans)
