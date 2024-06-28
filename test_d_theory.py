41cc719e10acd5a0eb5ba3e5c73d0ca61d3a9e3d
Allison Shi <ams9451@nyu.edu>
2024-06-28 13:55:16 -0600

    add d matrix

--- test_d_theory.py
+++ test_d_theory.py
@@ -10,6 +10,29 @@
 
 import numpy as np
 
-def gen_F(seq):
+# gamma(m, x, seq) is the largest value of r such that r <= x and 
+# seq[r] == seq[m], or 0 if no such value exists
+def gamma(m, x, seq):
+    for r in range(x, 0, -1):
+        if seq[r-1] == seq[m-1]:
+            return r
+    return 0
+
+def gen_D(seq):
     n = len(seq)
     f = np.zeros((n+1, n+1, n+1, n+1, n+1), dtype=int)      # f[m,j,i,k,l]
+    d = np.zeros((n+1, n+1, n+1, n+1, n+1), dtype=int)      # d[m,j,i,k,l]
+
+    for m in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(i+1, n+1):
+                for k in range(j, n+1):
+                    for l in range(k+1, m+1):
+                        f[m, j, i, k, l] = f[m - 1, j, i, k, l]
+                        if gamma(m, i, seq) > 0 and gamma(m, k, seq) >= j:
+                            f[m, j, i, k, l] = max(f[m, j, i, k, l], 
+                                                   f[m-1, j, gamma(m, i, seq)-1, gamma(m, k, seq)-1, l]+1)
+                        d[m, j, i, k, l] = f[m, j, i, k, l] - f[m, i-1, j, k, l]
+    return d
+    
+
