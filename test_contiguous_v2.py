637824b5f3680988ff378e2c941671418f0c734c
Allison Shi <ams9451@nyu.edu>
2024-07-03 10:38:29 -0600

    test_contig_l update, not passing some tests

---
+++ test_contiguous_v2.py
@@ -0,0 +1,69 @@
+# test contiguous 1s per j and per l value
+# j = i+1, l = k+1
+
+import numpy as np
+import random
+
+# characters allowed in test string 
+alphabet = ['a', 'b', 'c', 'd', 'e']
+
+# max size of the test string 
+max_length = 15
+
+# number of test cases
+num_tests = 10000
+
+def gamma(m, x, seq):
+    for r in range(x, 0, -1):
+        if seq[r-1] == seq[m-1]:
+            return r
+    return 0
+
+def check_l(l_vec): 
+    first_one_index = -1
+    last_one_index = -1
+    for x in range(len(l_vec)):
+      if l_vec[x] == 1:
+        if first_one_index == -1:
+            first_one_index = x
+            last_one_index = x
+        else: 
+            last_one_index = x
+    for y in range(first_one_index, last_one_index +1):
+        if l_vec[y] != 1:
+            return False
+    return True
+
+def gen_l(seq, count):
+    n = len(seq)
+    f = np.zeros((n+1, n+1, n+1, n+1, n+1), dtype=int)      # f[m,j,i,k,l]
+
+    # for each j, m, i, k
+    for m in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(i+1, n+1):
+                for k in range(j, n+1):
+                    l_vector = np.zeros((n+1), dtype=int)
+                    for l in range(k+1, m+1):
+                        f[m, j, i, k, l] = f[m - 1, j, i, k, l]
+                        if gamma(m, i, seq) > 0 and gamma(m, k, seq) >= j:
+                            f[m, j, i, k, l] = max(f[m, j, i, k, l], f[m-1, j, gamma(m, i, seq)-1, gamma(m, k, seq)-1, l]+1)
+
+                        l_vector[l] = f[m, j, i, k, l] - f[m, j, i-1, k, l]
+                    # l_vector = np.array([0, 1, 0, 1, 0])
+                    if np.any(l_vector) == 1 and not check_l(l_vector):
+                        print(f"Test {count}: Fail {seq}")
+                        print(f"m: {m}, j: {j}, i: {i}, k: {k}")
+                        print(l_vector)
+                        print("\n")
+                        return False
+    print(f"Test {count}: Pass {seq}")
+    return True
+        
+not_failed = True
+for x in range(num_tests):
+  if not_failed:
+    seq = ""
+    for _ in range(random.randint(max_length-5, max_length)):
+      seq += str(random.choice(alphabet))
+    not_failed = gen_l(seq, x)
