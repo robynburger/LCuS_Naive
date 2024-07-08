2b4996da768f1c1a5608216314266c8efe838729
robynburger <robynlburger@gmail.com>
2024-07-08 11:50:45 -0600

    remove gamma comment test_contiguous_j.py

--- test_contiguous_j.py
+++ test_contiguous_j.py
@@ -1,9 +1,6 @@
 import random
 import numpy as np
 
-# gamma(m, x, seq) is the largest value of r such that r <= x and 
-# seq[r] == seq[m], or 0 if no such value exists
-
 # characters allowed in test string 
 alphabet = ['a', 'b', 'c']
 
